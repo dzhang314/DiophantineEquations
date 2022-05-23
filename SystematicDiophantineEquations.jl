c33313d6238887f3dd8b33d83ce85b9ba2f6b52b
David K. Zhang <dzhang314@gmail.com>
2022-05-22 22:20:48 -0700

    eliminate linear polynomials and polynomials with trivial roots

--- SystematicDiophantineEquations.jl
+++ SystematicDiophantineEquations.jl
@@ -1,7 +1,8 @@
 module SystematicDiophantineEquations
 
 export Monomial, Polynomial,
-    all_polynomials, reduced_polynomials, unique_polynomials
+    all_polynomials, reduced_polynomials, unique_polynomials,
+    nontrivial_polynomials, unique_nontrivial_polynomials
 
 using Graphs
 using Singular
@@ -235,6 +236,88 @@ end
 
 function unique_polynomials(::Val{N}, height::Int) where {N}
     polys = reduced_polynomials(Val{N}(), height)
+    index_dict = Dict{Polynomial{N}, Int}()
+    for (i, p) in enumerate(polys)
+        index_dict[sort(p)] = i
+    end
+    g = SimpleGraph(length(polys))
+    for (i, p) in enumerate(polys)
+        add_edge!(g, i, index_dict[sort!(apply_transposition(p))])
+        add_edge!(g, i, index_dict[sort!(apply_negation(p))])
+        add_edge!(g, i, index_dict[sort!(apply_cycle(p))])
+        add_edge!(g, i, index_dict[sort!(apply_minus(p))])
+    end
+    _, vars = PolynomialRing(ZZ, ["" for _ = 1 : N])
+    x = ntuple(i -> (@inbounds vars[i]), Val{N}())
+    result = Polynomial{N}[]
+    for comp in connected_components(g)
+        @inbounds p = polys[comp[1]]
+        if length(factor(p(x)).fac) == 1
+            push!(result, p)
+        end
+    end
+    return result
+end
+
+################################################################################
+
+function has_trivial_root(p::Polynomial{N}) where {N}
+    for x in Iterators.product(ntuple(_ -> -1:1, Val{N}())...)
+        if p(x) == 0
+            return true
+        end
+    end
+    return false
+end
+
+function has_trivial_variable(p::Polynomial{N}, i::Int) where {N}
+    @inbounds for (_, m) in p
+        if m[i] != 0
+            if m[i] != 1
+                return false
+            end
+            for j = 1 : N
+                if i != j && m[j] != 0
+                    return false
+                end
+            end
+        end
+    end
+    return true
+end
+
+function has_trivial_variable(p::Polynomial{N}) where {N}
+    for i = 1 : N
+        if has_trivial_variable(p, i)
+            return true
+        end
+    end
+    return false
+end
+
+function nontrivial_polynomials(::Val{N}, height::Int) where {N}
+    result = Polynomial{N}[]
+    for partition in power_of_two_partitions(height)
+        if partition[end][2] == 0
+            product_space = Iterators.product([
+                homogeneous_polynomials(Val{N}(), i, j)
+                for (i, j) in partition
+            ]...)
+            for point in product_space
+                poly = reduce(vcat, point)
+                if (uses_all_variables(poly) && coefficient_gcd(poly) == 1
+                                             && !has_trivial_variable(poly)
+                                             && !has_trivial_root(poly))
+                    push!(result, poly)
+                end
+            end
+        end
+    end
+    return result
+end
+
+function unique_nontrivial_polynomials(::Val{N}, height::Int) where {N}
+    polys = nontrivial_polynomials(Val{N}(), height)
     index_dict = Dict{Polynomial{N}, Int}()
     for (i, p) in enumerate(polys)
         index_dict[sort(p)] = i
