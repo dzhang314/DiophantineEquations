451c160174503d21131c839d79eb2d5cbf779938
David K. Zhang <dzhang314@gmail.com>
2022-05-24 14:59:00 -0700

    fix edge cases (N=0, N=1) in solver program

--- Solve.jl
+++ Solve.jl
@@ -4,18 +4,9 @@ using Singular
 
 push!(LOAD_PATH, @__DIR__)
 using SystematicDiophantineEquations
+using SystematicDiophantineEquations: canonical_variables
 
 ################################################################################
-
-function canonical_variables(n::Int)
-    if n <= 3
-        return string.(Vector{Char}("xyz"[1:n]))
-    elseif n <= 26
-        return string.(Vector{Char}("abcdefghijklmnopqrstuvwxyz"[1:n]))
-    else
-        error()
-    end
-end
 
 function to_smt2(n::Int)
     if signbit(n)
@@ -27,7 +18,7 @@ end
 
 function to_smt2(coeff::Int, powers::NTuple{N, Int},
                  vars::Vector{String}) where {N}
-    if sum(powers) == 0
+    if N == 0 || sum(powers) == 0
         return to_smt2(coeff)
     else
         pieces = [to_smt2(coeff)]
@@ -45,8 +36,14 @@ function to_smt2(p::Polynomial{N}, vars::Vector{String}) where {N}
     for var in vars
         push!(result, "(declare-const $var Int)")
     end
-    p_expr = join([to_smt2(c, m, vars) for (c, m) in p], ' ')
-    push!(result, "(assert (= (+ $p_expr) 0))")
+    if length(p) > 1
+        p_expr = join([to_smt2(c, m, vars) for (c, m) in p], ' ')
+        push!(result, "(assert (= (+ $p_expr) 0))")
+    elseif length(p) == 1
+        push!(result, "(assert (= $(to_smt2(p[1]..., vars)) 0))")
+    else
+        push!(result, "(assert (= 0 0))")
+    end
     push!(result, "(check-sat)\n")
     return join(result, '\n')
 end
@@ -68,6 +65,24 @@ function to_wolfram(p::Polynomial{N}) where {N}
         vars,
         W"Integers"
     )
+end
+
+function run_wolfram(p::Polynomial{0})
+    if length(p) == 0
+        c = 0
+    else
+        c = p[1][1]
+    end
+    start = time_ns()
+    result = weval(W"Equal"(c, 0))
+    stop = time_ns()
+    if result == W"True"
+        return (SAT, stop - start)
+    elseif result == W"False"
+        return (UNSAT, stop - start)
+    else
+        error()
+    end
 end
 
 function run_wolfram(p::Polynomial{N}) where {N}
@@ -126,45 +141,44 @@ end
 
 ################################################################################
 
-function main(::Val{N}) where {N}
-    var_names = canonical_variables(N)
-    _, vars = PolynomialRing(ZZ, var_names)
-    x = ntuple(i -> (@inbounds vars[i]), Val{N}())
-    run_wolfram([(1, ntuple(_ -> 1, Val{N}()))]) # precompile wolfram
-    for height = 1 : 99
-        polys = unique_nontrivial_polynomials(Val{N}(), height)
-        if length(polys) > 0
-            println("----------------------------------------------------------------------------------------")
-            @printf("---------------------- NON-TRIVIAL HEIGHT %02d %d-VARIABLE EQUATIONS ----------------------\n", height, N)
-            println("----------------------------------------------------------------------------------------")
-            println()
-            println(" WOLFRAM    CVC5      Z3   YICES MATHSAT |  WOLFRAM     CVC5       Z3    YICES  MATHSAT")
-            println("-----------------------------------------+----------------------------------------------+")
-            for p in polys
-                wolfram_result, wolfram_time = run_wolfram(p)
-                cvc5_result, cvc5_time = run_smt2(CVC5_CMD, p)
-                z3_result, z3_time = run_smt2(Z3_CMD, p)
-                yices_result, yices_time = run_smt2(YICES_CMD, p)
-                mathsat_result, mathsat_time = run_smt2(MATHSAT_CMD, p)
-                println(
-                    lpad(wolfram_result, 8),
-                    lpad(cvc5_result, 8),
-                    lpad(z3_result, 8),
-                    lpad(yices_result, 8),
-                    lpad(mathsat_result, 8),
-                    " | ",
-                    @sprintf("%8.6f", 1.0e-9 * wolfram_time),
-                    @sprintf("%9.6f", 1.0e-9 * cvc5_time),
-                    @sprintf("%9.6f", 1.0e-9 * z3_time),
-                    @sprintf("%9.6f", 1.0e-9 * yices_time),
-                    @sprintf("%9.6f", 1.0e-9 * mathsat_time),
-                    " | ", p(x), " == 0"
-                )
-                flush(stdout)
+function main()
+    for height = 1 : 12
+        println("----------------------------------------------------------------------------------------")
+        @printf("------------------------- ALL HEIGHT %03d DIOPHANTINE EQUATIONS -------------------------\n", height)
+        println("----------------------------------------------------------------------------------------")
+        println()
+        println(" WOLFRAM    CVC5      Z3   YICES MATHSAT |  WOLFRAM     CVC5       Z3    YICES  MATHSAT")
+        println("-----------------------------------------+----------------------------------------------+")
+        for N = 0 : div(height, 2)
+            run_wolfram([(1, ntuple(_ -> 1, Val{N}()))]) # precompile weval
+            polys = dedup(all_polynomials(Val{N}(), height), false)
+            if length(polys) > 0
+                for p in polys
+                    wolfram_result, wolfram_time = run_wolfram(p)
+                    cvc5_result, cvc5_time = run_smt2(CVC5_CMD, p)
+                    z3_result, z3_time = run_smt2(Z3_CMD, p)
+                    yices_result, yices_time = run_smt2(YICES_CMD, p)
+                    mathsat_result, mathsat_time = run_smt2(MATHSAT_CMD, p)
+                    println(
+                        lpad(wolfram_result, 8),
+                        lpad(cvc5_result, 8),
+                        lpad(z3_result, 8),
+                        lpad(yices_result, 8),
+                        lpad(mathsat_result, 8),
+                        " | ",
+                        @sprintf("%8.6f", 1.0e-9 * wolfram_time),
+                        @sprintf("%9.6f", 1.0e-9 * cvc5_time),
+                        @sprintf("%9.6f", 1.0e-9 * z3_time),
+                        @sprintf("%9.6f", 1.0e-9 * yices_time),
+                        @sprintf("%9.6f", 1.0e-9 * mathsat_time),
+                        " | ", degrevlex_string(p), " == 0"
+                    )
+                    flush(stdout)
+                end
             end
-            println()
         end
+        println()
     end
 end
 
-main(Val{parse(Int, ARGS[1])}())
+main()
