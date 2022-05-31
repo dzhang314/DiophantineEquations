03b6637bcbb3dff554138b19c325effaaf467c78
David K. Zhang <dzhang314@gmail.com>
2022-05-30 22:00:47 -0700

    add predicate to find_polynomials

--- PolynomialAnalysis.jl
+++ PolynomialAnalysis.jl
@@ -3,57 +3,7 @@ module PolynomialAnalysis
 export dedup, to_wolfram, has_real_root, is_positive_definite
 
 using DZPolynomials
-using Graphs
 using MathLink
-
-################################################################################
-
-function dedup(polys::Vector{Polynomial{T, N, I}},
-               ::Type{I}, # needed to disambiguate when N = 0
-               verbose::Bool=false) where {T, N, I}
-    if verbose
-        println("Deduplicating $(length(polys)) polynomials.")
-        flush(stdout)
-    end
-    index_dict = Dict{Polynomial{T, N, I}, Int}()
-    for (i, p) in enumerate(polys)
-        index_dict[sort(p)] = i
-    end
-    if verbose
-        println("Constructing symmetry map.")
-        flush(stdout)
-    end
-    last_print = time_ns()
-    g = SimpleGraph(index_dict.count)
-    for (i, p_original) in enumerate(polys)
-        p = copy(p_original)
-        add_edge!(g, i, index_dict[sort!(apply_transposition!(p))])
-        apply_transposition!(p)
-        add_edge!(g, i, index_dict[sort!(apply_negation!(p))])
-        apply_negation!(p)
-        add_edge!(g, i, index_dict[sort!(apply_signflip!(p))])
-        apply_signflip!(p)
-        add_edge!(g, i, index_dict[sort!(apply_cycle!(p))])
-        if verbose && (time_ns() >= last_print + 1000000000)
-            println("Computed symmetries of $i polynomials.")
-            flush(stdout)
-            last_print += 1000000000
-        end
-    end
-    if verbose
-        println("Finding connected components of symmetry graph.")
-        flush(stdout)
-    end
-    result = Polynomial{T, N, I}[]
-    @inbounds for comp in connected_components(g)
-        push!(result, polys[comp[1]])
-    end
-    if verbose
-        println("Finished deduplicating.")
-        flush(stdout)
-    end
-    return result
-end
 
 ################################################################################
 
