d4a0bf0b4b2ee86776a2f69a134cb6f9ffab6842
David K. Zhang <dzhang314@gmail.com>
2022-07-06 18:44:42 -0700

    Update ListAll.jl

--- ListAll.jl
+++ ListAll.jl
@@ -7,42 +7,36 @@ using DZPolynomialSearch
 
 function inner(::Val{N}, height::T, partition::Vector{Pair{I, T}},
                i::Int, n::Int) where {T, N, I}
+    lockname = @sprintf("All-%02d-%02d-%04d.lock", height, N, i)
+    donename = @sprintf("All-%02d-%02d-%04d.done", height, N, i)
+    tempname = @sprintf("All-%02d-%02d-%04d.temp", height, N, i)
     filename = @sprintf("All-%02d-%02d-%04d.jls", height, N, i)
-    @assert !isfile(filename)
-    println("Working on partition $partition ($i out of $n).")
-    flush(stdout)
-    touch(filename)
-    polys = all_polynomials(Val{N}(), partition, true)
-    rm(filename)
-    serialize(filename, polys)
-    return filename
+    if !isfile(lockname) && !isfile(donename)
+        touch(lockname)
+        println("Working on partition $partition ($i out of $n).")
+        flush(stdout)
+        polys = all_polynomials(Val{N}(), partition, true)
+        serialize(tempname, polys)
+        mv(tempname, filename)
+        mv(lockname, donename)
+    end
 end
 
 function main()
     for height = 0 : 99
-        for N = 0 : min(div(height, 2), 6)
-            dataname = @sprintf("All-%02d-%02d.jls", height, N)
-            lockname = @sprintf("All-%02d-%02d.lock", height, N)
-            donename = @sprintf("All-%02d-%02d.done", height, N)
-            if !isfile(lockname) && !isfile(donename)
-                touch(lockname)
-                println("Searching for all $N-variable polynomials of height $height.")
-                flush(stdout)
-                partitions = [
-                    partition
-                    for partition in binary_partitions(Int8(height), UInt8)
-                    if sum([i * j for (i, j) in partition]) >= N
-                ]
-                n = length(partitions)
-                println("Found $n binary partitions of $height.")
-                flush(stdout)
-                filenames = String[]
-                for (i, partition) in enumerate(partitions)
-                    push!(filenames, inner(Val{N}(), Int8(height), partition, i, n))
-                end
-                serialize(dataname, deserialize.(filenames))
-                rm.(filenames)
-                mv(lockname, donename)
+        for N = 0 : div(height, 2)
+            println("Searching for all $N-variable polynomials of height $height.")
+            flush(stdout)
+            partitions = [
+                partition
+                for partition in binary_partitions(Int8(height), UInt8)
+                if sum([i * j for (i, j) in partition]) >= N
+            ]
+            n = length(partitions)
+            println("Found $n binary partitions of $height.")
+            flush(stdout)
+            for (i, partition) in enumerate(partitions)
+                inner(Val{N}(), Int8(height), partition, i, n)
             end
         end
     end
