dfc35f869024a917a8da2994860f5505c54d84b7
David K. Zhang <dzhang314@gmail.com>
2022-07-06 18:51:47 -0700

    Update ListAll.jl

--- ListAll.jl
+++ ListAll.jl
@@ -25,18 +25,22 @@ end
 function main()
     for height = 0 : 99
         for N = 0 : div(height, 2)
-            println("Searching for all $N-variable polynomials of height $height.")
-            flush(stdout)
-            partitions = [
-                partition
-                for partition in binary_partitions(Int8(height), UInt8)
-                if sum([i * j for (i, j) in partition]) >= N
-            ]
-            n = length(partitions)
-            println("Found $n binary partitions of $height.")
-            flush(stdout)
-            for (i, partition) in enumerate(partitions)
-                inner(Val{N}(), Int8(height), partition, i, n)
+            donename = @sprintf("All-%02d-%02d.done", height, N)
+            if !isfile(donename)
+                println("Searching for all $N-variable polynomials of height $height.")
+                flush(stdout)
+                partitions = [
+                    partition
+                    for partition in binary_partitions(Int8(height), UInt8)
+                    if sum([i * j for (i, j) in partition]) >= N
+                ]
+                n = length(partitions)
+                println("Found $n binary partitions of $height.")
+                flush(stdout)
+                for (i, partition) in enumerate(partitions)
+                    inner(Val{N}(), Int8(height), partition, i, n)
+                end
+                touch(donename)
             end
         end
     end
