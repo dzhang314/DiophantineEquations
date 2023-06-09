bc6d6400b9207fc356dec842ea774157e6793f1b
David K. Zhang <dzhang314@gmail.com>
2023-06-09 01:40:07 -0700

    begin work on new C++ implementation

---
+++ Monomial.hpp
@@ -0,0 +1,93 @@
+#ifndef DIOPHANTINE_EQUATIONS_MONOMIAL_HPP_INCLUDED
+#define DIOPHANTINE_EQUATIONS_MONOMIAL_HPP_INCLUDED
+
+#include <array>   // for std::array
+#include <cstddef> // for std::size_t
+#include <cstdint> // for std::intNN_t, std::uintNN_t
+#include <vector>  // for std::vector
+
+namespace DiophantineEquations {
+
+
+using T_COEFF = std::int16_t;
+using T_EXPONENT = std::uint8_t;
+
+
+template <std::size_t NUM_VARS>
+struct Monomial {
+
+    std::array<T_EXPONENT, NUM_VARS> exponents;
+
+    explicit constexpr Monomial() noexcept
+        : exponents() {
+        for (std::size_t i = 0; i < NUM_VARS; ++i) {
+            exponents[i] = static_cast<T_EXPONENT>(0);
+        }
+    }
+
+    constexpr T_EXPONENT get_exponent(std::size_t i) const noexcept {
+        return exponents[i];
+    }
+
+    constexpr void set_exponent(std::size_t i, T_EXPONENT value) noexcept {
+        exponents[i] = value;
+    }
+
+    /**
+     * Compute and return a list of all possible monomials of total degree
+     * `degree` in `NUM_VARS` variables. The returned list is sorted in
+     * degrevlex order.
+     */
+    static constexpr std::vector<Monomial> all_monomials(T_EXPONENT degree
+    ) noexcept {
+        std::vector<Monomial<NUM_VARS>> result;
+        if constexpr (NUM_VARS == 0) {
+            if (degree == 0) { result.emplace_back(); }
+        } else if constexpr (NUM_VARS == 1) {
+            Monomial<1> monomial;
+            monomial.set_exponent(0, degree);
+            result.push_back(monomial);
+        } else {
+            // To produce monomials in degrevlex order, iterate over
+            // possible values of the exponent of the *last* variable.
+            for (T_EXPONENT last = 0; last <= degree; ++last) {
+                // Recursively compute all possible monomials of the
+                // remaining total degree in `NUM_VARS - 1` variables.
+                const std::vector<Monomial<NUM_VARS - 1>> heads =
+                    Monomial<NUM_VARS - 1>::all_monomials(degree - last);
+                for (const Monomial<NUM_VARS - 1> &head : heads) {
+                    Monomial<NUM_VARS> monomial;
+                    for (std::size_t i = 0; i < NUM_VARS - 1; ++i) {
+                        monomial.set_exponent(i, head.get_exponent(i));
+                    }
+                    monomial.set_exponent(NUM_VARS - 1, last);
+                    result.push_back(monomial);
+                }
+            }
+        }
+        return result;
+    }
+
+}; // struct Monomial
+
+
+// template <std::size_t NUM_VARS>
+// struct Term {
+
+//     Monomial<NUM_VARS> monomial;
+//     T_COEFF coefficient;
+
+// }; // struct Term
+
+
+// template <std::size_t NUM_VARS>
+// struct Polynomial {
+
+//     std::vector<Term<NUM_VARS>> terms;
+
+// }; // struct Polynomial
+
+
+} // namespace DiophantineEquations
+
+#endif // DIOPHANTINE_EQUATIONS_MONOMIAL_HPP_INCLUDED
---
+++ Partitions.hpp
@@ -0,0 +1,118 @@
+#ifndef DIOPHANTINE_EQUATIONS_PARTITIONS_HPP_INCLUDED
+#define DIOPHANTINE_EQUATIONS_PARTITIONS_HPP_INCLUDED
+
+#include <utility> // for std::move, std::pair, std::make_pair
+#include <vector>  // for std::vector
+
+namespace DiophantineEquations {
+
+
+/**
+ * Given a list of non-negative integers, compute another list of non-negative
+ * integers, having the same sum as the given list, which is next smallest in
+ * lexicographic order. The result is returned in-place by modifying the input.
+ */
+template <typename T>
+constexpr bool decrement_partition(std::vector<T> &partition) noexcept {
+
+    constexpr T ZERO = static_cast<T>(0);
+    constexpr T ONE = static_cast<T>(1);
+    using index_t = typename std::vector<T>::size_type;
+    const index_t n = partition.size();
+
+    // Partitions of length 0 and 1 are unique, so they cannot be incremented.
+    if (n <= 1) { return false; }
+
+    // If the next-to-last element is positive, decrement it and increment the
+    // final element.
+    if (partition[n - 2] > 0) {
+        partition[n - 2] -= ONE;
+        partition[n - 1] += ONE;
+        return true;
+    }
+
+    // Otherwise, scan backwards until we find a positive element.
+    for (index_t i = n - 2; i > 0; --i) {
+        // If we find one, decrement it and move the value of the final element
+        // backwards into the next element.
+        if (partition[i - 1] > 0) {
+            partition[i - 1] -= ONE;
+            partition[i] = partition[n - 1] + ONE;
+            partition[n - 1] = ZERO;
+            return true;
+        }
+    }
+
+    // If we find no positive elements, then we already have the
+    // lexicographically smallest partition, which cannot be decremented.
+    return false;
+}
+
+
+/**
+ * Given integers `i` and `k`, compute and return a list of all possible ways
+ * to express `k` as a sum of powers of two, up to and including `1 << i`.
+ *
+ * The result is encoded as a collection of lists of pairs, as follows:
+ * Each element `(m, n)` of an output list represents `m` copies of `1 << n`.
+ * For example, `{(3, 1), (1, 4), (0, 1)}` represents `{8, 2, 2, 2, 2, 1}`.
+ */
+template <typename T_INDEX, typename T_VALUE>
+std::vector<std::vector<std::pair<T_INDEX, T_VALUE>>>
+binary_partitions(T_INDEX i, T_VALUE k) {
+
+    // Base case: When `i == 0`, the only possible result is to express `k`
+    // as a sum of `k` copies of `1`.
+    if (i == 0) {
+        if (k == 0) {
+            return {{}};
+        } else {
+            return {{std::make_pair(static_cast<T_INDEX>(0), k)}};
+        }
+    }
+
+    // Recursive case: Iterate over all possible values for the number of
+    // occurrences of `1 << i` in the resulting partition.
+    const T_VALUE value = static_cast<T_VALUE>(1 << i);
+    std::vector<std::vector<std::pair<T_INDEX, T_VALUE>>> result;
+    for (T_VALUE first = k / value; first > 0; --first) {
+        // Recursively compute all possible partitions of the remaining value
+        // using powers of two smaller than `1 << i`.
+        const std::vector<std::vector<std::pair<T_INDEX, T_VALUE>>> tails =
+            binary_partitions<T_INDEX, T_VALUE>(i - 1, k - first * value);
+        for (const std::vector<std::pair<T_INDEX, T_VALUE>> &tail : tails) {
+            std::vector<std::pair<T_INDEX, T_VALUE>> partition;
+            partition.emplace_back(i, first);
+            partition.insert(partition.end(), tail.begin(), tail.end());
+            result.push_back(std::move(partition));
+        }
+    }
+
+    const std::vector<std::vector<std::pair<T_INDEX, T_VALUE>>> rest =
+        binary_partitions<T_INDEX, T_VALUE>(i - 1, k);
+    result.insert(result.end(), rest.begin(), rest.end());
+    return result;
+}
+
+
+/**
+ * Given an integer `k`, compute and return a list of all possible ways
+ * to express `k` as a sum of powers of two.
+ *
+ * The result is encoded as a collection of lists of pairs, as follows:
+ * Each element `(m, n)` of an output list represents `m` copies of `1 << n`.
+ * For example, `{(3, 1), (1, 4), (0, 1)}` represents `{8, 2, 2, 2, 2, 1}`.
+ */
+template <typename T_INDEX, typename T_VALUE>
+std::vector<std::vector<std::pair<T_INDEX, T_VALUE>>>
+binary_partitions(T_VALUE k) {
+    // Find the largest value of `i` such that `1 << i` is at most `k`.
+    T_INDEX i = static_cast<T_INDEX>(0);
+    while (static_cast<T_VALUE>(1 << i) <= k) { ++i; }
+    return binary_partitions<T_INDEX, T_VALUE>(--i, k);
+}
+
+
+} // namespace DiophantineEquations
+
+#endif // DIOPHANTINE_EQUATIONS_PARTITIONS_HPP_INCLUDED
