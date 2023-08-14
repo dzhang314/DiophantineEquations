24da1cb081b5bc5064cbd0441e974d00fda2fda0
David K. Zhang <dzhang314@gmail.com>
2023-08-14 00:56:13 -0700

    combine RootSearch and ModulusSearch into one script

--- scripts/RootSearch.jl
+++ scripts/RootSearch.jl
@@ -3,6 +3,25 @@ push!(LOAD_PATH, @__DIR__)
 using DiophantinePolynomials
 
 
+function structure_search!(
+    lines::Vector{String},
+    unsolved::Vector{Pair{Int,DiophantinePolynomial{N}}}
+) where {N}
+    to_delete = Int[]
+    for (j, (i, p)) in enumerate(unsolved)
+        if is_positive_definite(p)
+            lines[i] = "$(p) : positive-definite"
+            push!(to_delete, j)
+        elseif is_negative_definite(p)
+            lines[i] = "$(p) : negative-definite"
+            push!(to_delete, j)
+        end
+    end
+    deleteat!(unsolved, to_delete)
+    return nothing
+end
+
+
 function next_partition!(x::Vector{T}) where {T}
     _one = one(T)
     @inbounds begin
@@ -103,7 +122,101 @@ function l1_ball(::Val{N}, radius::T) where {N,T}
 end
 
 
-function main(::Val{N}, path::String, radius::T) where {N,T}
+function root_search!(
+    lines::Vector{String},
+    unsolved::Vector{Pair{Int,DiophantinePolynomial{N}}},
+    k::Int
+) where {N}
+    ball = l1_ball(Val{N}(), k)
+    to_delete = Int[]
+    for (j, (i, p)) in enumerate(unsolved)
+        root = find_root(p, ball)
+        if !isnothing(root)
+            lines[i] = "$(p) : $(root)"
+            push!(to_delete, j)
+        end
+    end
+    deleteat!(unsolved, to_delete)
+    return length(ball)
+end
+
+
+struct ModularInteger{T,U} <: Number
+    value::T
+    modulus::T
+end
+
+
+@inline function Base.:+(
+    x::ModularInteger{T,U}, y::ModularInteger{T,U}
+) where {T,U}
+    k = x.modulus
+    @assert k == y.modulus
+    @assert zero(T) <= x.value < k
+    @assert zero(T) <= y.value < k
+    result = x.value + y.value
+    return ModularInteger{T,U}(ifelse(result >= k, result - k, result), k)
+end
+
+
+@inline function Base.:*(
+    x::ModularInteger{T,U}, y::ModularInteger{T,U}
+) where {T,U}
+    k = x.modulus
+    @assert k == y.modulus
+    @assert zero(T) <= x.value < k
+    @assert zero(T) <= y.value < k
+    result = (U(x.value) * U(y.value)) % U(k)
+    return ModularInteger{T,U}(result % T, k)
+end
+
+
+function has_root_modulo(
+    p::DiophantinePolynomial{N}, k::T, ::Type{U}
+) where {N,T,U}
+    range = zero(T):k-one(T)
+    for x in Iterators.product(ntuple(_ -> range, Val{N}())...)
+        result = ModularInteger{T,U}(zero(T), k)
+        for (c, m) in p.terms
+            c_m = ModularInteger{T,U}(mod(c, k) % T, k)
+            x_m = ModularInteger{T,U}.(powermod.(x, m, k), k)
+            result += *(c_m, x_m...)
+        end
+        if iszero(result.value)
+            return true
+        end
+    end
+    return false
+end
+
+
+function modulus_search!(
+    lines::Vector{String},
+    unsolved::Vector{Pair{Int,DiophantinePolynomial{N}}},
+    k::Int
+) where {N}
+    to_delete = Int[]
+    for (j, (i, p)) in enumerate(unsolved)
+        if !has_root_modulo(p, k, UInt64)
+            lines[i] = "$(p) : modulus $(k)"
+            push!(to_delete, j)
+        end
+    end
+    deleteat!(unsolved, to_delete)
+    return nothing
+end
+
+
+function write_atomic(lines::Vector{String}, path::String)
+    temp_path, io = mktemp()
+    for line in lines
+        println(io, line)
+    end
+    mv(temp_path, path; force=true)
+end
+
+
+function main(::Val{N}, path::String) where {N}
 
     println("Reading file ", path, "...")
     flush(stdout)
@@ -124,33 +237,71 @@ function main(::Val{N}, path::String, radius::T) where {N,T}
         return nothing
     end
 
-    for k = zero(T):radius
-        ball = l1_ball(Val{N}(), k)
-        to_delete = Int[]
-        for (j, (i, p)) in enumerate(unsolved)
-            root = find_root(p, ball)
-            if !isnothing(root)
-                lines[i] = "$(p) : $(root)"
-                push!(to_delete, j)
-            end
+    let
+
+        old_length = length(unsolved)
+        structure_search!(lines, unsolved)
+        new_length = length(unsolved)
+
+        println("Solved $(old_length - new_length) equations by structural analysis.")
+        println("$(new_length) unsolved equations remain.")
+        flush(stdout)
+
+        if old_length != new_length
+            write_atomic(lines, path)
         end
-        deleteat!(unsolved, to_delete)
+
         if isempty(unsolved)
-            break
+            return nothing
         end
+
     end
 
-    println(length(unsolved), " unsolved equations remain.")
+    next_modulus = 2
+    for k = zero(Int):typemax(Int)
 
-    temp_path, io = mktemp()
-    for line in lines
-        println(io, line)
+        old_length = length(unsolved)
+        ball_size = root_search!(lines, unsolved, k)
+        new_length = length(unsolved)
+
+        println("Solved $(old_length - new_length) equations at radius $(k).")
+        println("$(new_length) unsolved equations remain.")
+        flush(stdout)
+
+        if old_length != new_length
+            write_atomic(lines, path)
+        end
+
+        if isempty(unsolved)
+            return nothing
+        end
+
+        while next_modulus^N <= ball_size
+
+            old_length = length(unsolved)
+            modulus_search!(lines, unsolved, next_modulus)
+            new_length = length(unsolved)
+
+            println("Solved $(old_length - new_length) equations at modulus $(next_modulus).")
+            println("$(new_length) unsolved equations remain.")
+            flush(stdout)
+
+            if old_length != new_length
+                write_atomic(lines, path)
+            end
+
+            if isempty(unsolved)
+                return nothing
+            end
+
+            next_modulus += 1
+
+        end
+
     end
 
-    mv(temp_path, path; force=true)
     return nothing
-
 end
 
 
-main(Val(parse(Int, ARGS[1])), ARGS[3], parse(Int, ARGS[2]))
+main(Val(parse(Int, ARGS[1])), ARGS[2])
