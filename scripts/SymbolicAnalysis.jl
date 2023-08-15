b8f5a1ebe159f5b1ef952b77b76d62c0d35bcb97
David K. Zhang <dzhang314@gmail.com>
2023-08-14 19:05:53 -0700

    add has_real_root, has_compact_locus

--- scripts/SymbolicAnalysis.jl
+++ scripts/SymbolicAnalysis.jl
@@ -1,9 +1,9 @@
 module SymbolicAnalysis
 
-using MathLink: @W_str, WSymbol, WExpr
+using MathLink: @W_str, WSymbol, WExpr, weval
 using DiophantinePolynomials: DiophantinePolynomial
 
-export has_compact_projection_expr
+################################################################################
 
 const IntExpr = Union{Int,WSymbol,WExpr}
 
@@ -15,6 +15,23 @@ const WOLFRAM_ALPHABET = [
 
 const WOLFRAM_RADIUS = W"R"
 const WOLFRAM_NEGATIVE_RADIUS = W"Times"(-1, WOLFRAM_RADIUS)
+
+################################################################################
+
+function wolfram_variables(::Val{N}) where {N}
+    @assert 1 <= N <= 26
+    base = (1 <= N <= 26) ? 23 : 0
+    return ntuple(i -> (@inbounds WOLFRAM_ALPHABET[base+i]), Val{N}())
+end
+
+function wolfram_bool(s::WSymbol)
+    if s.name == "True"
+        return true
+    else
+        @assert s.name == "False"
+        return false
+    end
+end
 
 function wolfram_product(factors::Vector{IntExpr})
     if isempty(factors)
@@ -52,10 +69,60 @@ function wolfram_polynomial(
     return W"Plus"(terms...)
 end
 
+################################################################################
+
+export has_real_root
+
+function has_real_root_expr(p::DiophantinePolynomial{N}) where {N}
+    vars = wolfram_variables(Val{N}())
+    return W"Exists"(W"List"(vars...),
+        W"Equal"(wolfram_polynomial(p, vars), 0)
+    )
+end
+
+has_real_root(p::DiophantinePolynomial{N}) where {N} =
+    wolfram_bool(weval(W"Resolve"(has_real_root_expr(p), W"Reals")))
+
+################################################################################
+
+export has_compact_locus
+
+function has_compact_locus_expr(p::DiophantinePolynomial{N}) where {N}
+    vars = wolfram_variables(Val{N}())
+    clauses = ntuple(
+        i -> W"LessEqual"(WOLFRAM_NEGATIVE_RADIUS, vars[i], WOLFRAM_RADIUS),
+        Val{N}()
+    )
+    return W"Exists"(WOLFRAM_RADIUS, W"ForAll"(W"List"(vars...),
+        W"Implies"(W"Equal"(wolfram_polynomial(p, vars), 0), W"And"(clauses...))
+    ))
+end
+
+function has_compact_locus_expr(
+    p::DiophantinePolynomial{N}, radius::Int
+) where {N}
+    vars = wolfram_variables(Val{N}())
+    clauses = ntuple(
+        i -> W"LessEqual"(-radius, vars[i], radius),
+        Val{N}()
+    )
+    return W"ForAll"(W"List"(vars...),
+        W"Implies"(W"Equal"(wolfram_polynomial(p, vars), 0), W"And"(clauses...))
+    )
+end
+
+has_compact_locus(p::DiophantinePolynomial{N}) where {N} =
+    wolfram_bool(weval(W"Resolve"(has_compact_locus_expr(p), W"Reals")))
+
+has_compact_locus(p::DiophantinePolynomial{N}, radius::Int) where {N} =
+    wolfram_bool(weval(W"Resolve"(has_compact_locus_expr(p, radius), W"Reals")))
+
+################################################################################
+
+export has_compact_projection
+
 function has_compact_projection_expr(p::DiophantinePolynomial{N}) where {N}
-    @assert 1 <= N <= 26
-    base = (1 <= N <= 26) ? 23 : 0
-    vars = ntuple(i -> (@inbounds WOLFRAM_ALPHABET[base+i]), Val{N}())
+    vars = wolfram_variables(Val{N}())
     clauses = ntuple(
         i -> W"LessEqual"(WOLFRAM_NEGATIVE_RADIUS, vars[i], WOLFRAM_RADIUS),
         Val{N}()
@@ -64,5 +131,28 @@ function has_compact_projection_expr(p::DiophantinePolynomial{N}) where {N}
         W"Implies"(W"Equal"(wolfram_polynomial(p, vars), 0), W"Or"(clauses...))
     ))
 end
+
+function has_compact_projection_expr(
+    p::DiophantinePolynomial{N}, radius::Int
+) where {N}
+    vars = wolfram_variables(Val{N}())
+    clauses = ntuple(
+        i -> W"LessEqual"(-radius, vars[i], radius),
+        Val{N}()
+    )
+    return W"ForAll"(W"List"(vars...),
+        W"Implies"(W"Equal"(wolfram_polynomial(p, vars), 0), W"Or"(clauses...))
+    )
+end
+
+has_compact_projection(p::DiophantinePolynomial{N}) where {N} =
+    wolfram_bool(weval(W"Resolve"(has_compact_projection_expr(p), W"Reals")))
+
+has_compact_projection(p::DiophantinePolynomial{N}, radius::Int) where {N} =
+    wolfram_bool(weval(
+        W"Resolve"(has_compact_projection_expr(p, radius), W"Reals")
+    ))
+
+################################################################################
 
 end # module SymbolicAnalysis
